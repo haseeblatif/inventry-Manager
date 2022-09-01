9ed3de8889de23bbc8c359e0609221d7fe854a74
haseeblatif <mhaseebsahar@gmail.com>
2022-09-01 23:58:03 +0500

    admin logout

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,6 +16,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+
+Route::controller(AdminController::class)->group(function(){
+    Route::get('/admin/logout', 'destroy')->name('admin.logout');
+
 });
 
 Route::get('/dashboard', function () {
