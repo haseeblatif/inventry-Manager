be4656744b3e56627c7fa80b5effe387994b10d6
haseeblatif <mhaseebsahar@gmail.com>
2022-09-09 15:27:31 +0500

    Edit Admin Profile Part 2

--- resources/views/admin/admin_profile_edit.blade.php
+++ resources/views/admin/admin_profile_edit.blade.php
@@ -1,5 +1,6 @@
 @extends('admin.admin_master')
 @section('admin')
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 
 <div class="page-content">
     <div class="container-fluid">
@@ -30,14 +31,14 @@
             <div class="row mb-3">
                 <label for="example-text-input" class="col-sm-2 col-form-label">Profile Image</label>
                 <div class="col-sm-10">
-                    <input name="profile_image" class="form-control" type="file"  value="{{ $adminData->username }}"  id="example-text-input">
+                    <input name="profile_image" class="form-control" type="file"  value="{{ $adminData->username }}"  id="image">
                 </div>
             </div>
             <div class="row mb-3">
                 <label for="example-text-input" class="col-sm-2 col-form-label"></label>
                 <div class="col-sm-10">
                    
-                        <img class="rounded-circle avatar-xl" alt="200x200" src="{{ asset('backend/assets/images/users/avatar-4.jpg') }}" data-holder-rendered="true">
+                        <img id="showImage" class="rounded-circle avatar-xl" alt="200x200" src="{{ asset('backend/assets/images/users/avatar-4.jpg') }}" data-holder-rendered="true">
                 </div>
             </div>
             <input type="submit" class="btn btn-info waves-effect waves-light" value="Update Profile">
@@ -47,8 +48,27 @@
             <!-- end row -->
         </div>
 
-
     </div>
 </div>
+    </div>
+  </div>
+</div>
+
+<script type="text/javascript">
+   
+   $(document).ready(function(){
+    $('#image').change(function(e){
+        var reader = new FileReader();
+        reader.onload = function(e){
+            $('#showImage').attr('src',e.target.result);
+        }
+        reader.readAsDataURL(e.target.files['0']);
+    });
+   });
+
+</script>
+
+
+
 
 @endsection
